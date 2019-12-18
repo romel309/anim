81efd5fab055adf7043352148c09c225dd862657
romel_mar <A01207360@itesm.mx>
2019-12-18 17:18:23 -0600

    logout

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace App\Http\Controllers\Auth;
-
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+use Session;
 
 class LoginController extends Controller
 {
@@ -35,5 +37,22 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    protected function credentials(Request $request)
+    {
+        $field = filter_var($request->get($this->username()), FILTER_VALIDATE_EMAIL)
+        ? $this->username()
+        : 'username';
+
+        return [
+        $field => $request->get($this->username()),
+        'password' => $request->password,
+        ];
+    }
+
+    public function logout(Request $request) {
+      Auth::logout();
+      return redirect('/');
     }
 }
